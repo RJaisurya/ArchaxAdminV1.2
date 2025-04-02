f1c863ffb8692d8e0f35f8d64a3e3e40dfc0db7e
jaisurya <jaisuryarajasekhar@gmail.com>
2025-04-02 18:24:33 +0530

    3rd commit

--- tests/AdminLogin.spec.js
+++ tests/AdminLogin.spec.js
@@ -1,10 +1,12 @@
 import{test,expect} from '@playwright/test';
 import {Login} from '../pages/Login';
-import {CreaOrg} from '../pages/CreateOrganization'
+import {CreaOrg} from '../pages/CreateOrganization';
+import {Subadmin} from '../pages/Subadmin';
+
 
 test('AdminLogin',async({page})=>{
 //View in Full page
-await page.setViewportSize({ width: 1920, height: 1080 });
+// await page.setViewportSize({ width: 1920, height: 1080 });
 
 //Login Into the Admin Page
 const login=new Login(page);
@@ -33,5 +35,22 @@ await org.adminMailInput("jais@yopmail.com");
 await page.waitForTimeout(1000);
 await org.saveOrg();
 await page.waitForTimeout(5000);
+await org.dashboard();
+
+//Sub-admin Creation
+const subadmin=new Subadmin(page);
+await subadmin.gotoSubAdmin();
+await subadmin.addname("Jai");
+await subadmin.ademail("jaya@yopmail.com");
+await subadmin.adrole("Admin");
+await subadmin.adaccess("All")
+await subadmin.savebtn();
+await page.waitForTimeout(5000);
+await subadmin.gotoDash();
+
+
+
+
+
 
 })
